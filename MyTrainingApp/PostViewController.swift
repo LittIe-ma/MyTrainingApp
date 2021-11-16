68219371fc87a94c7b1f5fabbf1c5cbacdc7a013
yasu1125 <yasudaM84@gmail.com>
2021-11-16 22:31:17 +0900

    投稿画面を作成

--- MyTrainingApp/PostViewController.swift
+++ MyTrainingApp/PostViewController.swift
@@ -9,10 +9,35 @@ import UIKit
 
 class PostViewController: UIViewController {
 
+    @IBOutlet private weak var postTextView: UITextView!
+    @IBOutlet private weak var profileImageView: UIImageView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         setStatusBarbackgroundColor(.green)
+        profileImageView.setCircle()
+        setPostButton()
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        postTextView.becomeFirstResponder()
+    }
+
+    private func setPostButton() {
+        let postButton = UIButton(frame: CGRect(x: 0, y: 0, width: 80, height: 30))
+        postButton.backgroundColor = .purple
+        postButton.tintColor = .white
+        postButton.setTitle("Post", for: .normal)
+        postButton.layer.cornerRadius = 15
+        postButton.addTarget(self, action: #selector(self.didTapPost), for: .touchUpInside)
+        let item = UIBarButtonItem(customView: postButton)
+        self.navigationItem.rightBarButtonItem = item
+    }
+
+    @objc private func didTapPost() {
+        print("button tapped")
+    }
 }
