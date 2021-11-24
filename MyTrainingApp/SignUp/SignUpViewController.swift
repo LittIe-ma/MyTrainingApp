a4c2ab049ae66bd01675a6da8db3d112c57cdea6
yasu1125 <yasudaM84@gmail.com>
2021-11-24 22:24:48 +0900

    ディレクトリ構成変更

---
+++ MyTrainingApp/SignUp/SignUpViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpViewController.swift
+//  MyTrainingApp
+//
+//  Created by yasudamasato on 2021/11/24.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
