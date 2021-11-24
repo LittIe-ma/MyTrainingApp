d2a80ffde49386b7d74322b0ea2446ece9b20783
yasu1125 <yasudaM84@gmail.com>
2021-11-24 22:00:48 +0900

    IQKeyboardManagerSwiftライブラリ追加

--- MyTrainingApp/AppDelegate.swift
+++ MyTrainingApp/AppDelegate.swift
@@ -6,15 +6,17 @@
 //
 
 import UIKit
+import IQKeyboardManagerSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
         Router.shared.showRoot(window: UIWindow(frame: UIScreen.main.bounds))
+
+        IQKeyboardManager.shared.enable = true
+
         return true
     }
 }
