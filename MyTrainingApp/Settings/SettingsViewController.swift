cec3afad83cbc4443c28a9fc9f5a05ff9c2c672c
yasu1125 <yasudaM84@gmail.com>
2021-11-29 11:52:44 +0900

    仮ログアウトボタンを追加

--- MyTrainingApp/Settings/SettingsViewController.swift
+++ MyTrainingApp/Settings/SettingsViewController.swift
@@ -17,5 +17,19 @@ class SettingsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        testLogoutButton()
+    }
+
+    private func testLogoutButton() {
+        let button = UIButton()
+        button.frame = CGRect(x: self.view.frame.width/4, y: self.view.frame.height/2, width: self.view.frame.width/2, height: 50)
+        button.setTitle("Logout", for: .normal)
+        button.backgroundColor = .green
+        button.addTarget(self, action: #selector(didTapLogout(_:)), for: .touchUpInside)
+        self.view.addSubview(button)
+    }
+
+    @objc private func didTapLogout(_ sender: Any) {
+
     }
 }
