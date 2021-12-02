544e356f7f11ad7ff325e7a9900ba8a0ff62cc9b
yasu1125 <yasudaM84@gmail.com>
2021-12-02 13:01:15 +0900

    不要なコードを削除

--- MyTrainingApp/Router/Router.swift
+++ MyTrainingApp/Router/Router.swift
@@ -18,10 +18,6 @@ final class Router {
     func showRoot(window: UIWindow?) {
         if Auth.auth().currentUser?.uid != nil {
             let tabBarController = TabBarController.makeFromStoryboard()
-//            let navigationController = UINavigationController(rootViewController: tabBarController)
-//            navigationController.navigationBar.barTintColor = .green
-//            navigationController.navigationBar.backgroundColor = .green
-//            navigationController.setStatusBarbackgroundColor(.green)
             window?.rootViewController = tabBarController
             window?.makeKeyAndVisible()
             self.window = window
